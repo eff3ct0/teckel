1bba0994c8f751fe56d07be78b15979584ad4f81
Rafael Fernández <40717893+rafafrdz@users.noreply.github.com>
2024-12-23 19:23:12 +0000

    Fix Sonatype CI/CD (#19)

--- project/SonatypePublish.scala
+++ project/SonatypePublish.scala
@@ -1,14 +1,16 @@
 import sbt.Keys._
 import sbt._
+import xerial.sbt.Sonatype.autoImport.{sonatypeCredentialHost, sonatypeRepository}
+import xerial.sbt.Sonatype.sonatypeCentralHost
 
 import scala.collection.Seq
 
 object SonatypePublish {
 
   def projectSettings: Seq[Setting[_]] = Seq(
-    ThisBuild / publish / skip       := true,
-    ThisBuild / versionScheme        := Some("early-semver"),
-    ThisBuild / pomIncludeRepository := { _ => false }
+    ThisBuild / publish / skip         := true,
+    ThisBuild / versionScheme          := Some("early-semver"),
+    ThisBuild / sonatypeCredentialHost := sonatypeCentralHost
   )
 
 }
