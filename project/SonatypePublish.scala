f64f82ddf0162fc21e720a636c70374439a84453
Rafael Fernández <github@rafaelfernandez.dev>
2024-12-23 20:18:24 +0100

    fix

--- project/SonatypePublish.scala
+++ project/SonatypePublish.scala
@@ -1,6 +1,7 @@
 import sbt.Keys._
 import sbt._
 import xerial.sbt.Sonatype.autoImport.{sonatypeCredentialHost, sonatypeRepository}
+import xerial.sbt.Sonatype.sonatypeCentralHost
 
 import scala.collection.Seq
 
@@ -9,8 +10,7 @@ object SonatypePublish {
   def projectSettings: Seq[Setting[_]] = Seq(
     ThisBuild / publish / skip         := true,
     ThisBuild / versionScheme          := Some("early-semver"),
-    ThisBuild / sonatypeCredentialHost := "s01.oss.sonatype.org",
-    sonatypeRepository                 := "https://s01.oss.sonatype.org/service/local"
+    ThisBuild / sonatypeCredentialHost := sonatypeCentralHost
   )
 
 }
